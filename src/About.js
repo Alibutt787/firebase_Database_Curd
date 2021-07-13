7f4b0b9fea89ab8704ecc4f2bd522429e6f69c84
AQIB Ali Butt <67646902+Alibutt787@users.noreply.github.com>
2021-07-14 02:12:49 +0500

    Update About.js

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ export const About = () => {
     return (
         
         <div style={{display:'flex',flexDirection:'column', justifyContent:'center', textAlign:'center' , width:'100%'}}>
-          <div style={{marginTop:'100px'}}> <h4 > Coming Soon ! Go to </h4>
+          <div style={{marginTop:'100px'}}> <h4 > Coming Soon !!! </h4>
         <span> Go Back to   <Link style={{ color:'green'}} to="/"> <b>Home</b></Link> </span>
         </div>
         </div>
