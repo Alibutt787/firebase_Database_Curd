c24136ca5ccec288daeedc33903ada842df3cbab
Aqib <aqibbuttbutt7867@gmail.com>
2021-08-09 23:00:31 -0700

    8th

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCHFomLFq0yUiHeMsDoioUoE-qPIIEZt7w",
-    authDomain: "curdpractise.firebaseapp.com",
-    projectId: "curdpractise",
-    storageBucket: "curdpractise.appspot.com",
-    messagingSenderId: "829455785337",
-    appId: "1:829455785337:web:d4f874d15bf74cc3dea3a8",
-    measurementId: "G-QGEJTK7RP8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  export default firebase;
-   
+  apiKey: "AIzaSyCHFomLFq0yUiHeMsDoioUoE-qPIIEZt7w",
+  authDomain: "curdpractise.firebaseapp.com",
+  projectId: "curdpractise",
+  storageBucket: "curdpractise.appspot.com",
+  messagingSenderId: "829455785337",
+  appId: "1:829455785337:web:d4f874d15bf74cc3dea3a8",
+  measurementId: "G-QGEJTK7RP8",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
